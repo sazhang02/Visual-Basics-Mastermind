9b665b3ec169967c18e42878799419f150ab3ea2
Samantha Zhang <sammie@dhcp-clara-1083.eduroam.cornell.edu>
2020-02-05 15:54:50 -0500

    First commit

---
+++ Mastermind/Mastermind/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblEnter = New System.Windows.Forms.Label()
+        Me.lblColorsC = New System.Windows.Forms.Label()
+        Me.lblPositionsC = New System.Windows.Forms.Label()
+        Me.lblColors = New System.Windows.Forms.Label()
+        Me.lblPositions = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.txtGuessOne = New System.Windows.Forms.TextBox()
+        Me.txtGuessTwo = New System.Windows.Forms.TextBox()
+        Me.txtGuessThree = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblEnter
+        '
+        Me.lblEnter.AutoSize = True
+        Me.lblEnter.Location = New System.Drawing.Point(16, 42)
+        Me.lblEnter.Name = "lblEnter"
+        Me.lblEnter.Size = New System.Drawing.Size(67, 13)
+        Me.lblEnter.TabIndex = 0
+        Me.lblEnter.Text = "Enter Colors:"
+        '
+        'lblColorsC
+        '
+        Me.lblColorsC.AutoSize = True
+        Me.lblColorsC.Location = New System.Drawing.Point(16, 104)
+        Me.lblColorsC.Name = "lblColorsC"
+        Me.lblColorsC.Size = New System.Drawing.Size(81, 13)
+        Me.lblColorsC.TabIndex = 1
+        Me.lblColorsC.Text = "Colors correct ="
+        '
+        'lblPositionsC
+        '
+        Me.lblPositionsC.AutoSize = True
+        Me.lblPositionsC.Location = New System.Drawing.Point(16, 136)
+        Me.lblPositionsC.Name = "lblPositionsC"
+        Me.lblPositionsC.Size = New System.Drawing.Size(95, 13)
+        Me.lblPositionsC.TabIndex = 2
+        Me.lblPositionsC.Text = "Positions Correct ="
+        '
+        'lblColors
+        '
+        Me.lblColors.AutoSize = True
+        Me.lblColors.Location = New System.Drawing.Point(117, 104)
+        Me.lblColors.Name = "lblColors"
+        Me.lblColors.Size = New System.Drawing.Size(37, 13)
+        Me.lblColors.TabIndex = 3
+        Me.lblColors.Text = "_____"
+        '
+        'lblPositions
+        '
+        Me.lblPositions.AutoSize = True
+        Me.lblPositions.Location = New System.Drawing.Point(117, 136)
+        Me.lblPositions.Name = "lblPositions"
+        Me.lblPositions.Size = New System.Drawing.Size(37, 13)
+        Me.lblPositions.TabIndex = 4
+        Me.lblPositions.Text = "_____"
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(167, 197)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(89, 23)
+        Me.btnCheck.TabIndex = 5
+        Me.btnCheck.Text = "Check Guess"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'txtGuessOne
+        '
+        Me.txtGuessOne.Location = New System.Drawing.Point(92, 39)
+        Me.txtGuessOne.Name = "txtGuessOne"
+        Me.txtGuessOne.Size = New System.Drawing.Size(47, 20)
+        Me.txtGuessOne.TabIndex = 6
+        '
+        'txtGuessTwo
+        '
+        Me.txtGuessTwo.Location = New System.Drawing.Point(145, 39)
+        Me.txtGuessTwo.Name = "txtGuessTwo"
+        Me.txtGuessTwo.Size = New System.Drawing.Size(53, 20)
+        Me.txtGuessTwo.TabIndex = 7
+        '
+        'txtGuessThree
+        '
+        Me.txtGuessThree.Location = New System.Drawing.Point(204, 39)
+        Me.txtGuessThree.Name = "txtGuessThree"
+        Me.txtGuessThree.Size = New System.Drawing.Size(53, 20)
+        Me.txtGuessThree.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtGuessThree)
+        Me.Controls.Add(Me.txtGuessTwo)
+        Me.Controls.Add(Me.txtGuessOne)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.lblPositions)
+        Me.Controls.Add(Me.lblColors)
+        Me.Controls.Add(Me.lblPositionsC)
+        Me.Controls.Add(Me.lblColorsC)
+        Me.Controls.Add(Me.lblEnter)
+        Me.Name = "Form1"
+        Me.Text = "Mastermind"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblEnter As System.Windows.Forms.Label
+    Friend WithEvents lblColorsC As System.Windows.Forms.Label
+    Friend WithEvents lblPositionsC As System.Windows.Forms.Label
+    Friend WithEvents lblColors As System.Windows.Forms.Label
+    Friend WithEvents lblPositions As System.Windows.Forms.Label
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents txtGuessOne As System.Windows.Forms.TextBox
+    Friend WithEvents txtGuessTwo As System.Windows.Forms.TextBox
+    Friend WithEvents txtGuessThree As System.Windows.Forms.TextBox
+
+End Class
---
+++ Mastermind/Mastermind/Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+
+    Dim intOne As Integer
+    Dim intTwo As Integer
+    Dim intThree As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Randomize()
+        intOne = Int(Rnd() * 4 + 1)
+        intTwo = Int(Rnd() * 4 + 1)
+        intThree = Int(Rnd() * 4 + 1)
+
+    End Sub
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+        Dim str1 As String
+        Dim str2 As String
+        Dim str3 As String
+
+        Dim intguess1 As Integer
+        Dim intguess2 As Integer
+        Dim intguess3 As Integer
+
+        str1 = txtGuessOne.Text
+        str2 = txtGuessTwo.Text
+        str3 = txtGuessThree.Text
+
+        If str1 = "R" Then
+            intguess1 = 1
+        ElseIf str1 = "G" Then
+            intguess1 = 2
+        ElseIf str1 = "B" Then
+            intguess1 = 3
+        ElseIf str1 = "Y" Then
+            intguess1 = 4
+        End If
+
+
+        If str2 = "R" Then
+            intguess2 = 1
+        ElseIf str2 = "G" Then
+            intguess2 = 2
+        ElseIf str2 = "B" Then
+            intguess2 = 3
+        ElseIf str2 = "Y" Then
+            intguess2 = 4
+        End If
+
+
+        If str3 = "R" Then
+            intguess3 = 1
+        ElseIf str3 = "G" Then
+            intguess3 = 2
+        ElseIf str3 = "B" Then
+            intguess3 = 3
+        ElseIf str3 = "Y" Then
+            intguess3 = 4
+        End If
+
+
+        Dim intp As Integer = 0
+        Dim intc As Integer = 0
+
+
+        If (intOne = intguess1) Then
+            intp = intp + 1
+        End If
+        If (intTwo = intguess2) Then
+            intp = intp + 1
+        End If
+        If (intThree = intguess3) Then
+            intp = intp + 1
+        End If
+        lblPositions.Text = intp
+
+
+        Dim gflag1 As Boolean
+        Dim gflag2 As Boolean
+        Dim gflag3 As Boolean
+
+        gflag1 = False
+        gflag2 = False
+        gflag3 = False
+
+        If (intOne = intguess1) Then
+            intc = intc + 1
+            gflag1 = True
+        ElseIf (intOne = intguess2) Then
+            intc = intc + 1
+            gflag2 = True
+        ElseIf (intOne = intguess3) Then
+            intc = intc + 1
+            gflag3 = True
+        End If
+
+        If ((intTwo = intguess1) And (Not gflag1)) Then
+            intc = intc + 1
+            gflag1 = True
+        ElseIf ((intTwo = intguess2) And (Not gflag2)) Then
+            intc = intc + 1
+            gflag2 = True
+        ElseIf ((intTwo = intguess3) And (Not gflag3)) Then
+            intc = intc + 1
+            gflag3 = True
+        End If
+
+        If ((intThree = intguess1) And (Not gflag1)) Then
+            intc = intc + 1
+            gflag1 = True
+        ElseIf ((intThree = intguess2) And (Not gflag2)) Then
+            intc = intc + 1
+            gflag2 = True
+        ElseIf ((intThree = intguess3) And (Not gflag3)) Then
+            intc = intc + 1
+            gflag3 = True
+        End If
+
+        lblColors.Text = intc
+
+    End Sub
+End Class
